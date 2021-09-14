7a8b04c4535d08d87579d0351741ac555dc1c485
daemyung jang <dm86.jang@gmail.com>
2021-09-14 12:31:23 +0900

    Implement EGL_TEST

---
+++ Base/include/Base/utility.h
@@ -0,0 +1,27 @@
+//
+// Created by Daemyung Jang on 2021/09/12.
+//
+
+#ifndef UTILITY_H
+#define UTILITY_H
+
+#include <spdlog/spdlog.h>
+#include <EGL/egl.h>
+
+#define STRING(x) #x
+
+#ifndef NDEBUG
+#define EGL_TEST(function) do { \
+    function; \
+    EGLint error = eglGetError(); \
+    if (error != EGL_SUCCESS) { \
+        spdlog::error("{} with 0x{:x}.", STRING(function), error); \
+        throw std::runtime_error("Err to call EGL function."); \
+    } \
+} while(false)
+#else
+#define EGL_TEST(function) \
+    function
+#endif
+
+#endif //UTILITY_H
